6f8dc601bf1d1e05524ef7cc7afc726d4ecccef5
Michael <m051412369811@gmail.com>
2025-06-25 03:14:41 +0800

    "新增後端城市API路徑"+"把環境變數加入github repo"+"部屬內容新增讀取repo環境變數"

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -8,26 +8,26 @@ const model = ref([
         label: 'Home',
         items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
     },
-    {
-        label: 'UI Components',
-        items: [
-            { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
-            { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
-            { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
-            { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
-            { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
-            { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
-            { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
-            { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
-            { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
-            { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu' },
-            { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
-            { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
-            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
-            { label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/uikit/timeline' },
-            { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
-        ]
-    },
+    // {
+    //     label: 'UI Components',
+    //     items: [
+    //         { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
+    //         { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
+    //         { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
+    //         { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
+    //         { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
+    //         { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
+    //         { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
+    //         { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
+    //         { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
+    //         { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu' },
+    //         { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
+    //         { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
+    //         { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
+    //         { label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/uikit/timeline' },
+    //         { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
+    //     ]
+    // },
     {
         label: 'Pages',
         icon: 'pi pi-fw pi-briefcase',
@@ -59,11 +59,11 @@ const model = ref([
                     }
                 ]
             },
-            {
-                label: 'Crud',
-                icon: 'pi pi-fw pi-pencil',
-                to: '/pages/crud'
-            },
+            // {
+            //     label: 'Crud',
+            //     icon: 'pi pi-fw pi-pencil',
+            //     to: '/pages/crud'
+            // },
             {
                 label: '員工列表',
                 icon: 'pi pi-fw pi-pencil',
@@ -78,17 +78,17 @@ const model = ref([
                 label: '請假審核',
                 icon: 'pi pi-fw pi-pencil',
                 to: '/pages/leaveapproval'
-            },
-            {
-                label: 'Not Found',
-                icon: 'pi pi-fw pi-exclamation-circle',
-                to: '/pages/notfound'
-            },
-            {
-                label: 'Empty',
-                icon: 'pi pi-fw pi-circle-off',
-                to: '/pages/empty'
             }
+            // {
+            //     label: 'Not Found',
+            //     icon: 'pi pi-fw pi-exclamation-circle',
+            //     to: '/pages/notfound'
+            // },
+            // {
+            //     label: 'Empty',
+            //     icon: 'pi pi-fw pi-circle-off',
+            //     to: '/pages/empty'
+            // }
         ]
     },
     {
